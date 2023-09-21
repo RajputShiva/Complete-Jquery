26f61acc19b413a05f002de5383f72fc10c4a0ba
Shivaji Singh <shivajisingh54@gmail.com>
2023-09-21 09:09:08 +0530

    first commit

---
+++ All Events/windowEvents.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    // $(window).scroll(function() {
+    //     console.log("You are scrolling");
+    // });
+    // $('#box').scroll(function() {
+    //     console.log("You are scrolling");
+    // });
+
+    // $(window).resize(function() {
+    //     console.log("You are Resizing Window");
+    // });
+    
+});
